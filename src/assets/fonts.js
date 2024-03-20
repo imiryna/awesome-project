265f0a187a8d9df5e70fb8d89a8529c1a3f573e5
Iryna Burlaka <i.am.blinova@gmail.com>
2024-03-20 14:06:01 +0100

    import fonts

---
+++ src/assets/fonts.js
@@ -0,0 +1,7 @@
+import { useFonts } from "expo-font";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
+  });
+}
---
+++ src/components/text.js
@@ -0,0 +1,24 @@
+import { useFonts } from "expo-font";
+import { StyleSheet, Text } from "react-native";
+
+export const Text = () => {
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Text
+      styles={{
+        fontFamily: "Inter-Black",
+        fontSize: 30,
+      }}>
+      Open up App.js to start working on your app!
+    </Text>
+  );
+};
+
+// const styles = StyleSheet.create();
