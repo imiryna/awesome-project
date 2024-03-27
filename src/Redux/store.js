2614d913c17976ab092549b6875f14b910446f2e
Iryna Burlaka <i.am.blinova@gmail.com>
2024-03-27 17:22:21 +0100

    redux setup

---
+++ src/Redux/store.js
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+
+const authConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+    auth: persistReducer(authConfig, authReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
--- src/services/uathApi.js
+++ src/services/uathApi.js
@@ -1,5 +1,9 @@
 import { baseURLInstance } from "./baseUrl";
 
+export const setToken = (token) => {
+  baseURLInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
 export const userRegister = async (formData) => {
   const { data } = await baseURLInstance.post("/users/signup", formData);
   setToken(data.token);
